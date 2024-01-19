dd7fb1ebc44d149a3824df715f32c3e194867997
bitbird development <bs@bitbird.de>
2024-01-19 15:32:24 +0100

    convert to pimcore bundle + add autoloading for routes

--- src/OhDearBundle.php
+++ src/OhDearBundle.php
@@ -5,16 +5,31 @@ declare(strict_types=1);
 namespace bitbirddev\OhDearBundle;
 
 use bitbirddev\OhDearBundle\DependencyInjection\Compiler\CheckPass;
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 use function dirname;
 
-final class OhDearBundle extends Bundle
+final class OhDearBundle extends AbstractPimcoreBundle
 {
+    use PackageVersionTrait;
+
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+    protected function getComposerPackageName(): string
+    {
+        // getVersion() will use this name to read the version from
+        // PackageVersions and return a normalized value
+        return 'bitbirddev/oh-dear-bundle';
+    }
+
+    public function getNiceName(): string
+    {
+        return 'OhDear Bundle';
     }
 
     public function build(ContainerBuilder $container): void
